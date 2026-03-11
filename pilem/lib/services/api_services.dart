fcecfffdd6dca26dd66e8c3dd73a178f839bae9f
ErikaPutriSay <erikaputrisay_2327240097@mhs.mdp.ac.id>
2026-03-11 20:18:59 +0700

    update

--- pilem/lib/services/api_services.dart
+++ pilem/lib/services/api_services.dart
@@ -1,46 +1,64 @@
 import 'dart:convert';
-//import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'https://api.themoviedb.org/3';
-  //ganti dengan APIKey masing-masing
-  static const String apiKey = '9d6b1a75df5833e0e59a8534172adb39';
-  //1. mengambil list movie yang saat ini tayang
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = "9d6b1a75df5833e0e59a8534172adb39";
 
+  // 1. Mengambil daftar film yang tayang saat ini
   Future<List<Map<String, dynamic>>> getAllMovies() async {
     final response = await http.get(
-      Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"),
+      Uri.parse('$baseUrl/movie/now_playing?api_key=$apiKey'),
     );
-    final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['results']);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<Map<String, dynamic>>.from(data['results']);
+    } else {
+      throw Exception('Failed to load movies');
+    }
   }
 
-  //2. mengambil list movie yang sedang trending minggu ini
+  // 2. Mengambil List movie yang trending minggu ini
   Future<List<Map<String, dynamic>>> getTrendingMovies() async {
     final response = await http.get(
-      Uri.parse("$baseUrl/movie/trending/week?api_key=$apiKey"),
+      Uri.parse('$baseUrl/trending/movie/week?api_key=$apiKey'),
     );
-    final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['results']);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<Map<String, dynamic>>.from(data['results']);
+    } else {
+      throw Exception('Failed to load trending movies');
+    }
   }
 
-  //3. mengambil list popular movie 
+  // 3. Mengambil list pupular movie
   Future<List<Map<String, dynamic>>> getPopularMovies() async {
     final response = await http.get(
-      Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"),
+      Uri.parse('$baseUrl/movie/popular?api_key=$apiKey'),
     );
-    final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['results']);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<Map<String, dynamic>>.from(data['results']);
+    } else {
+      throw Exception('Failed to load popular movies');
+    }
   }
 
-  //4. mengambil list movie melalui pencarian
+  // 4. Mengambil list movie melalui pencarian
   Future<List<Map<String, dynamic>>> searchMovies(String query) async {
     final response = await http.get(
-      Uri.parse("$baseUrl/search/movie?api_key=$apiKey&query=$query"),
+      Uri.parse('$baseUrl/search/movie?query=$query&api_key=$apiKey'),
     );
-    final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['results']);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<Map<String, dynamic>>.from(data['results']);
+    } else {
+      throw Exception('Failed to search movies');
+    }
   }
 }
 //API Read Access Token :
