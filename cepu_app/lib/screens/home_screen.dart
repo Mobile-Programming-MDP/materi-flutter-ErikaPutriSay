b1a0856ce1bcded7de8099db75f74532ec441e02
ErikaPutriSay <erikaputrisay_2327240097@mhs.mdp.ac.id>
2026-04-08 12:03:42 +0700

    Update

--- cepu_app/lib/screens/home_screen.dart
+++ cepu_app/lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:cepu_app/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
